7f1938eb930ef7bacd32920c152309dd11452875
Ronald Xavier <ronaldx563@gmail.com>
2024-08-29 23:57:22 -0300

    Adicionando projeto biblioteca-django

---
+++ biblioteca/biblioteca/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+import core.views
+
+urlpatterns = [
+    path('livros/', core.views.livro_list_create, name='livros-list-create'),
+    path('livros/<int:pk>/', core.views.livro_detail, name='livro-detail'),
+]
+
+
+
+
+'''
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+'''
